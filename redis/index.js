c8117d42eb2eddf75bed34bfe30aaf3188c753b7
Kenneth Obikwelu <kenneth.obikwelu@altais.com>
2022-11-11 05:52:44 -0800

    remote redis client

--- redis/index.js
+++ redis/index.js
@@ -13,42 +13,46 @@ const getConnectionString = () => {
 
 
 exports.createClient = async () => {
-    if (state.connected) {
-        return;
-    }
-    if (config.redis.connectionString) {
-        redisClient = await redis.createClient({getConnectionString});
-        logger.info('***** REMOTE REDIS client created .....')
-       
+    try {
+        if (state.connected) {
+            return;
+        }
+        if (config.redis.connectionString) {
+            redisClient = await redis.createClient({getConnectionString});
+            logger.info('***** REMOTE REDIS client created .....')
 
-    } else {
-        redisClient = await redis.createClient({});
-        logger.info('***** LOCAL REDIS client created .....')
 
-        redisClient.on("error", (error) => {
-            logger.error('***** Not connected to REDIS .....')
-            logger.error(`Error : ${error}`)
+        } else {
+            redisClient = await redis.createClient({});
+            logger.info('***** LOCAL REDIS client created .....')
+
+            redisClient.on("error", (error) => {
+                logger.error('***** Not connected to REDIS .....')
+                logger.error(`Error : ${error}`)
+            });
+        }
+
+
+
+        await redisClient.connect()
+        logger.info('***** Connected to redis client .....')
+        if (redisClient) {
+            logger.info('***** connection verified .....')
+            state.connected = true;
+        }
+
+
+        redisClient.on("disconnected", () => {
+            logger.warn('Redis default connection is disconnected')
         });
+
+        process.on('SIGINT', function () {
+            logger.info('Redis default connection is disconnected due to application termination')
+            process.exit(0);
+        });
+        return redisClient
+    }catch(error){
+        logger.error(`Error : ${error}`)
     }
-
-
-
-    await redisClient.connect()
-    logger.info('***** Connected to redis client .....')
-    if (redisClient) {
-        logger.info('***** connection verified .....')
-        state.connected = true;
-    }
-
-
-    redisClient.on("disconnected", () => {
-        logger.warn('Redis default connection is disconnected')
-    });
-
-    process.on('SIGINT', function () {
-        logger.info('Redis default connection is disconnected due to application termination')
-        process.exit(0);
-    });
-    return redisClient
 }
 
