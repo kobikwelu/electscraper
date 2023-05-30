21fc4af23d810c027bf8f35494616f84f589a773
Kenneth Chidozie Obikwelu <kenneth.chidozieobikwelu@wwex.com>
2023-05-30 16:50:44 -0700

    fixing crontask

--- cronTask.js
+++ cronTask.js
@@ -4,15 +4,15 @@ const {totalWords, maxToken, postKeywords} = config
 
 
 async function runTask() {
-    logger.info(`generating new posts for today ${Date.now()}`);
+    console.log(`generating new posts for today ${Date.now()}`);
     try {
         let keywords = postKeywords.split(',')
         for (const keyword of keywords) {
             await contentController.generatePosts(keyword, maxToken, totalWords, 'unsplash');
         }
-        logger.info(`completed generating posts`);
+        console.log(`completed generating posts`);
     } catch (ex) {
-        logger.info(ex);
+        console.log(ex);
     }
 }
 
